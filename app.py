4c862caca74999b18b2510ca810c6298ba4b16c2
Aditya Narayan Mishra <dithere10@gmail.com>
2026-01-03 01:20:47 +0530

    Add new allowed origins for CORS

--- app.py
+++ app.py
@@ -111,7 +111,9 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
-        "http://localhost:8000"
+        "http://localhost:8000",
+        "https://splitsmart-backend2.onrender.com",
+        "http://localhost:8001"
         ],          # allow your frontend
     allow_credentials=True,
     allow_methods=["*"],            # GET, POST, etc.

a45b4d96aa9305b265051ec4c36a068b6431d948
Aditya Narayan Mishra <dithere10@gmail.com>
2026-04-09 15:25:11 +0530

    Add new allowed origin for frontend

--- app.py
+++ app.py
@@ -108,7 +108,8 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:8000",
         "https://splitsmart-backend2.onrender.com",
-        "http://localhost:8001"
+        "http://localhost:8001",
+        "https://splitsmart-frontend.onrender.com/"
         ],          # allow your frontend
     allow_credentials=True,
     allow_methods=["*"],            # GET, POST, etc.
